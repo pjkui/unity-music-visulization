e63b480f7f49a359ba14ab154743a23b6c8a9242
quinnpan(潘敬奎) <quinnpan@tencent.com>
2016-05-30 20:02:27 +0800

    add color

--- Assets/script/spectrum.cs
+++ Assets/script/spectrum.cs
@@ -9,6 +9,7 @@ public class spectrum : MonoBehaviour {
 	public float scale = 10.0f;
 	public float height = 0.3f;
 
+	private int freq=0;
 	// Use this for initialization
 	void Start () {
 		float angle = 2* Mathf.PI/objsNum;
@@ -23,12 +24,30 @@ public class spectrum : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float[] spectrum = AudioListener.GetSpectrumData(128,0,FFTWindow.Hamming);
+		if(++freq % 4 !=0){
+			
+			return ;
+		}
+		freq =0;
+		//float [] spectrum = new float[128];
+		//AudioListener.GetOutputData(spectrum,1);
+		float[] spectrum = AudioListener.GetOutputData(32,0);
 		for(int i=0;i<objsNum;++i)
 		{
-			Vector3 prePos = cubes[i].transform.localScale;
-			prePos.y = Mathf.Abs(spectrum[i]*scale) + height;
-			cubes[i].transform.localScale = prePos;
+			Vector3 preScale = cubes[i].transform.localScale;
+			preScale.y = Mathf.Abs(spectrum[i]*scale)+height;
+			cubes[i].transform.localScale = preScale;
+			Vector3 prePostion = cubes[i].transform.position;
+			prePostion.y = preScale.y/2;
+			cubes[i].transform.position = prePostion;
+
+			int red = (int)preScale.y;
+			if(red>255) red =255;
+			int g = Random.Range(0,255);
+			int b = Random.Range(0,255);
+			GameObject d = cubes[i];
+			var render = d.GetComponent<Renderer>();
+			render.material.color = new Color(red,g,b);
 		}
 	}
 }
